36f4c88c74d947c5f13e03185f2ae7d34b963934
jdbecerra <jdbecerra1202@users.noreply.github.com>
2021-08-31 19:43:35 -0500

    final frontend

--- src/App.js
+++ src/App.js
@@ -1,9 +1,82 @@
 import React from 'react';
+import './styles/form.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faUser, faEnvelope, faPhone, faMapMarkerAlt,faMapMarkedAlt} from '@fortawesome/free-solid-svg-icons';
+import {List} from './components/List'
 
 function App() {
   return (
-    <div>
-      
+    <div className="todo">
+      <div className="row-1">
+        <h4>DIRECCION DE ENVIO</h4>
+        <hr />
+        <form className="form-register">
+          
+          <div className="form-group">
+            <span className="icon"><FontAwesomeIcon icon ={faUser}/></span>
+            <input type="text" placeholder="Nombre" className="field"/>
+          </div>
+
+          <div className="form-group">
+            <span className="icon"><FontAwesomeIcon icon ={faUser}/></span>
+            <input type="text" placeholder="Apellidos" className="field"/>
+          </div>
+            
+          <div className="form-group">
+            <div className="fondo"></div>
+            <span className="icon"><FontAwesomeIcon icon ={faEnvelope}/></span>
+            <input type="email" placeholder="Correo Electronico" className="field"/>
+          </div>  
+
+          <div className="form-group">
+            <span className="icon"><FontAwesomeIcon icon ={faPhone}/></span>
+            <input type="text" placeholder="Numero de telefono" className="field"/>
+          </div>
+
+          <div className="form-group">
+            <span className="icon"><FontAwesomeIcon icon ={faMapMarkerAlt}/></span>
+            <input type="text" placeholder="Codigo Postal" className="field"/>
+          </div>
+
+          <div className="form-group">
+            <span className="icon"><FontAwesomeIcon icon ={faMapMarkerAlt}/></span>
+            <input type="text" placeholder="Colonia" className="field"/>
+          </div>
+
+          <div className="form-group">
+            <span className="icon"><FontAwesomeIcon icon ={faMapMarkerAlt}/></span>
+            <input type="text" placeholder="Estado/Region" className="field"/>
+          </div>
+
+          <div className="form-group">
+            <span className="icon"><FontAwesomeIcon icon ={faMapMarkerAlt}/></span>
+            <input type="text" placeholder="Ciudad" className="field"/>
+          </div>
+
+          <div className="form-group">
+            <span className="icon"><FontAwesomeIcon icon ={faMapMarkerAlt}/></span>
+            <input type="text" placeholder="Delegacion o municipio" className="field"/>
+          </div>
+
+          <div className="form-group">
+            <span className="icon"><FontAwesomeIcon icon ={faMapMarkedAlt}/></span>
+            <input type="text" placeholder="Calle" className="field"/>
+          </div>
+        </form>
+
+        <div className="buttons">
+            <button type="submit" className="btn-libretas">Libreta de direcciones</button>
+
+            <button type="submit" className="btn-guardar">Guardar</button>
+            <div className="check">
+              <label><input type="checkbox" />Utilizar como direccion de facturacion</label>
+            </div>
+        </div>
+      </div>
+
+      <div className="row-2">
+        <List/>
+      </div>
     </div>
   );
 }
--- src/components/List.js
+++ src/components/List.js
@@ -1,10 +1,65 @@
 import React from 'react';
+import image from '../assets/uMOPx1T.jpg'
 
 export class List extends React.Component{
     render(){
+        return(
         <div>
-            
+            <div className="list">
+                <div className="card">
+                    <div className="header">
+                        <h4><strong>RESUMEN DE LA ORDEN</strong></h4>
+                    </div>
+                    <div className="body">
+                        <div className="col1">
+                            <img src={image} />
+                        </div>
+                        <div className="col2">
+                            <p>COFRE LADY EMBLEM 75 ML EDP + 7,5 ML EDP + 100 ML B</p>
+                        </div>
+                        <div className="col3">
+                            <p><strong>$1890</strong></p>
+                        </div>
+                    </div>
+                    <div className="btn">
+                            <button type="submit" className="btn-editar"><strong>Editar</strong></button>
+                        </div>
+                    <div className="footer">
+                        <div className="subtotal">
+                            <div className="line1">
+                                <div className="col-1">
+                                    <p><strong>SUBTOTAL</strong></p>
+                                </div>
+                                <div className="col-2">
+                                    <p><strong>$13974</strong></p>
+                                </div>
+                            </div>
+
+                            <div className="line2">
+                                <div className="col-1">
+                                    <p><strong>ENVIO</strong></p>
+                                </div>
+                                <div className="col-2">
+                                    <p><strong>A calcular</strong></p>
+                                </div>
+                            </div>
+                            
+                        </div>
+                        <div className="total">
+                            <div className="col-1">
+                                <p><strong>TOTAL</strong></p>
+                            </div>
+                            <div className="col-2">
+                                <p><strong>$13974</strong></p>
+                            </div>
+                            
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
         </div>
+        )
     }
 }
 
